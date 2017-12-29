1953e0feef5ee38af2d7f97cf339a3c810dc337b
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-29 21:01:18 +0000

    Thermostat works and show temperature in London

--- src/thermostatjquery.js
+++ src/thermostatjquery.js
@@ -3,6 +3,11 @@ window.onload = function () {
   updatepage();
 };
 
+var usagecolor;
+var usagetext;
+var savercolor;
+var savertext;
+
 
 updatepage = function () {
   $("#temp").html(thermostat.currentTemperature)
@@ -11,6 +16,23 @@ updatepage = function () {
   $("#energysaverstat").css("background-color", savercolor)
   $("#energysaverstattext").html(savertext)
 }
+
+$.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=d5f6b97b7dc7c19a2b7e322588e9551d&units=metric', function(data){
+  $('#current-temperature').text(data.main.temp);
+})
+
+$.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=d5f6b97b7dc7c19a2b7e322588e9551d&units=metric', function(data){
+  $('#selectedcity').text(data.name);
+})
+
+
+$('#select-city').submit(function(event) {
+  event.preventDefault();
+  var city = $('#current-city').val();
+  displayWeather(city);
+})
+
+
 
 
 usagecolor = function (){
